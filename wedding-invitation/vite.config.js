40303ba2dbf995bf80736eecba72d9b91fe2c822
HuyIT <Huynguyen030704@gmail.com>
2025-12-22 13:45:06 +0700

    Sửa lại cấu hình deploy

--- wedding-invitation/vite.config.js
+++ wedding-invitation/vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Huynguyen030704/wedding-invitation-studio/",
+  base: "/wedding-invitation-studio/",
   plugins: [react()],
 });
